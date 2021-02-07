3ed959ffb21b8341e963429f28d945b5bc451bc7
manohar <manumanohar1527@gmail.com>
2021-02-07 14:01:09 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+
+const app = express();
+const PORT = process.env.PORT || 3030;
+
+//middeware's
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.json());
+
+app.get('/', (req, res)=>{
+    res.status(200).send("<h1>WhatsApp web </h1>");
+})
+
+app.listen(PORT, ()=>console.log(`server started at the port http://localhost:${PORT}`))
